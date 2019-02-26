9cacfe11b41a66606d9e64cbb75c891b9a7133b1
Akash Atharv <42795936+akashatharv@users.noreply.github.com>
2019-02-25 23:14:41 -0500

    change test

--- test.py
+++ test.py
@@ -32,13 +32,14 @@ def order_points(pts):
 
 def draw(img, imgpts):
     imgpts = np.int32(imgpts).reshape(-1,2)
-
+    print("imgpts")
+    print(imgpts)
     # draw ground floor in green
-    img = cv2.drawContours(img, [imgpts[:4]],-1,(0,255,0),-3)
+    img = cv2.drawContours(img, [imgpts[:4]],-1,(0,255,255),3)
 
     # draw pillars in blue color
     for i,j in zip(range(4),range(4,8)):
-        img = cv2.line(img, tuple(imgpts[i]), tuple(imgpts[j]),(255),3)
+        img = cv2.line(img, tuple(imgpts[i]), tuple(imgpts[j]),(255,255,0),3)
 
     # draw top layer in red color
     img = cv2.drawContours(img, [imgpts[4:]],-1,(0,0,255),3)
@@ -60,24 +61,27 @@ def homo(p1, p2):  # To generate the Homography
 
 def calc_mat(h):
     K=np.array([[1406.08415449821,0,0],[2.20679787308599, 1417.99930662800,0],[1014.13643417416, 566.347754321696,1]]).T
-    b_new = inv(K)*h
+    h = inv(h)
+    b_new = np.dot(inv(K),h)
     b1 = b_new[:,0].reshape(3,1)
     b2 = b_new[:,1].reshape(3,1)
-    r3 = np.cross(b_new[0,:],b_new[1,:])
+    r3 = np.cross(b_new[:,0],b_new[:,1])
+
     b3 = b_new[:,2].reshape(3,1)
     lambda_val = 2/(norm((inv(K)).dot(b1))+norm((inv(K)).dot(b2)))
     r1 = lambda_val*b1
     r2 = lambda_val*b2
-    r3 = lambda_val*lambda_val*r3.reshape(3,1)
+    r3 = (r3 * lambda_val * lambda_val).reshape(3, 1)
     t = lambda_val*b3
-    r = np.concatenate((r1,r2,r3),axis = 1)
+    r = np.concatenate((r1,r2,r3),axis=1)
+
     return r,t,K
 
 def homogenous_transform(image, pts):
     # obtain a consistent order of the points and unpack them
     # individually
     rect = order_points(pts)
-    max_dim = 160
+    max_dim = 170
 
     # camera coordiante points
     dst = np.array([
@@ -88,14 +92,14 @@ def homogenous_transform(image, pts):
 
     # compute the perspective transform matrix and then apply it
     m1 = homo(rect, dst)
-    warped_img = cv2.warpPerspective(image, m1, (max_dim, max_dim))
     r,t,K = calc_mat(m1)
     m2 = homo(dst, rect)  # For Lena on Tag
+    warped_img = cv2.warpPerspective(image, m1, (max_dim, max_dim))
 
     return warped_img,r,t,K,rect
 
 # Take the video
-cap = cv2.VideoCapture('Tag2.mp4')
+cap = cv2.VideoCapture('Tag0.mp4')
 
 
 while cap.isOpened():
@@ -110,7 +114,8 @@ while cap.isOpened():
     cnts = cv2.findContours(edged.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
     cnts = imutils.grab_contours(cnts)
     cnts = sorted(cnts, key=cv2.contourArea, reverse=True)[2:5]
-    axis = np.float32([[0,0,0], [0,159,0], [159,159,0], [159,0,0],[0,0,-159],[0,159,-159],[159,159,-159],[159,0,-159] ])
+    axis = np.float32([[0,0,0], [0,160,0], [160,160,0], [160,0,0],[0,0,-160],[0,160,-160],[160,160,-160],[160,0,-160] ])
+
 # loop over the contours
     for c in cnts:
         # approximate the contour
@@ -121,6 +126,7 @@ while cap.isOpened():
 
         if len(approx) == 4:
             corners = approx
+            print(corners)
             break
         elif len(approx) != 4:
             corners = None
@@ -131,13 +137,12 @@ while cap.isOpened():
         tag_count =  tag_count + 1
         cv2.drawContours(frame, [corners], -1, (0, 255, 0), 1)
         cv2.imshow("Outline", frame)
-        
+
         warped,r,t,K,rect = homogenous_transform(frame, corners[:, 0])
         cv2.imshow("Warped", warped)
-        
-        imgpts,jac = cv2.projectPoints(axis,r,t,K,np.zeros((1,8)))
-        img = draw(frame,imgpts)
 
+        imgpts,jac = cv2.projectPoints(axis,r,t,K,np.zeros((1,4)))
+        img = draw(frame, imgpts)
         cv2.imshow('Points',img)
 
     if cv2.waitKey(1) & 0xff == 27:
